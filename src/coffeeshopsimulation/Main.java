5c925a3d31f1419778f81ab711ee8320a3673285
Your Name <your.email@example.com>
2026-07-10 03:32:33 -0400

    Update simulation test results and customer settings

--- src/coffeeshopsimulation/Main.java
+++ src/coffeeshopsimulation/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-    	CoffeeShop coffeeShop = new CoffeeShop(3.0, 10);
+    	CoffeeShop coffeeShop = new CoffeeShop(4.0, 20);
 
         coffeeShop.startSimulation();
         coffeeShop.generateReport();
